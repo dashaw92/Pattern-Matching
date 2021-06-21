a9e69946c8ab58e3a6e94cb7516aa2bf32156e70
dashaw92 <dashaw92@gmail.com>
2021-06-21 05:14:38 -0400

    Make the methods more friendly to use Remove .gradle

--- src/main/java/me/danny/patterns/PatternBlock.java
+++ src/main/java/me/danny/patterns/PatternBlock.java
@@ -9,18 +9,3 @@ import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
-
-public final class PatternBlock<T> {
-
-    @SafeVarargs
-    public static <U> @NotNull PatternBlock<? extends U> block(@NotNull Function<? extends U, Object>... blocks) {
-        if(blocks == null || blocks.length == 0) throw new IllegalArgumentException("blocks must not be null or empty");
-        return new PatternBlock<>(Arrays.asList(blocks));
-    }
-
-    protected final List<Function<? extends T, Object>> blocks;
-
-    private PatternBlock(@NotNull  List<Function<? extends T, Object>> blocks) {
-        this.blocks = blocks;
-    }
-}
--- src/main/java/me/danny/patterns/PatternMatcher.java
+++ src/main/java/me/danny/patterns/PatternMatcher.java
@@ -4,12 +4,30 @@ import me.danny.patterns.test.DataImpl;
 import me.danny.patterns.test.EnumImpl;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
+import static me.danny.patterns.PatternMatcher.PatternBlock.run;
+import static me.danny.patterns.PatternMatcher.PatternSpec.specOf;
+
+/**
+ * Takes a {@link PatternSpec} and {@link PatternBlock} to be used to match against the parameter.
+ * The recommended way of consuming this API is to statically import the following methods:
+ * {@link PatternSpec#specOf(Class[])} {@link PatternBlock#run(Function[])} {@link PatternMatcher#match(PatternSpec, PatternBlock)}
+ * @see PatternMatcher#match(PatternSpec, PatternBlock)
+ * @see PatternMatcher#with(T) 
+ */
 public final class PatternMatcher<T> {
 
-    public static <U> @NotNull PatternMatcher<U> with(@NotNull PatternSpec<? extends U> patternSpec, @NotNull PatternBlock<? super U> block) {
+    /**
+     * @param patternSpec The patterns this matcher accepts
+     * @param block Associated blocks of code to be run by class type
+     * @param <U> The base class of the provided {@link PatternSpec}
+     * @return The new {@link PatternMatcher} constructed from the arguments
+     */
+    public static <U> @NotNull PatternMatcher<U> match(@NotNull PatternSpec<? extends U> patternSpec, @NotNull PatternBlock<? super U> block) {
         if(patternSpec.patterns.size() != block.blocks.size()) {
             throw new IllegalArgumentException("patternSpec.size() != block.size()! Cannot create a PatternMatcher with invalid arguments.");
         }
@@ -25,12 +43,17 @@ public final class PatternMatcher<T> {
         this.block = block;
     }
 
+    /**
+     * Match against the provided {@code instance}
+     * @param instance Object to match against
+     * @return The value the block of code associated with the type of the instance, or {@link Optional#empty()}
+     */
     @SuppressWarnings("unchecked")
-    public @NotNull Optional<Object> against(@NotNull T instance) {
+    public @NotNull Optional<?> with(@NotNull T instance) {
         for(int patIdx = 0; patIdx < spec.patterns.size(); patIdx++) {
             var pattern = spec.patterns.get(patIdx);
             if(pattern.equals(instance.getClass())) {
-                Function<T, Object> assocBlock = (Function<T, Object>) block.blocks.get(patIdx);
+                Function<? super T, ?> assocBlock = (Function<? super T, ?>) block.blocks.get(patIdx);
                 return Optional.ofNullable(assocBlock.apply(instance));
             }
         }
@@ -38,21 +61,74 @@ public final class PatternMatcher<T> {
         return Optional.empty();
     }
 
+    /**
+     * The mold for the pattern matcher.
+     * @param <T> Base class of all provided Classes
+     */
+    public static class PatternSpec<T> {
+
+        /**
+         * Factory method to create a PatternSpec.
+         * @param patternClasses List of classes
+         * @param <U> Base class of all provided classes
+         * @return A new PatternSpec instance
+         */
+        @SafeVarargs
+        public static <U> @NotNull  PatternSpec<U> specOf(@NotNull Class<? extends U>... patternClasses) {
+            if(patternClasses == null || patternClasses.length == 0) throw new IllegalArgumentException("patternClasses must not be null or empty");
+            return new PatternSpec<>(Arrays.asList(patternClasses));
+        }
+
+        protected final List<Class<? extends T>> patterns;
+
+        private PatternSpec(@NotNull List<Class<? extends T>> patterns) {
+            this.patterns = patterns;
+        }
+    }
+
+    /**
+     * List of lambdas to run per associated type
+     * @param <T> Base class of objects passed to the lambdas
+     */
+    public static final class PatternBlock<T> {
+
+        /**
+         * Factory method to create a PatternBlock.
+         * @param blocks List of lambdas
+         * @param <U> Base class of all provided classes
+         * @return A new PatternBlock instance
+         */
+        @SafeVarargs
+        public static <U> @NotNull PatternBlock<? extends U> run(@NotNull Function<? extends U, ?>... blocks) {
+            if(blocks == null || blocks.length == 0) throw new IllegalArgumentException("blocks must not be null or empty");
+            return new PatternBlock<>(Arrays.asList(blocks));
+        }
+
+        protected final List<Function<? extends T, ?>> blocks;
+
+        private PatternBlock(@NotNull  List<Function<? extends T, ?>> blocks) {
+            this.blocks = blocks;
+        }
+    }
+
+
     public static void main(String[] args) {
         var dataTest = new DataImpl("cool string!");
         var enumImpl = EnumImpl.TWO;
 
-        var pattern = PatternMatcher.with(PatternSpec.of(
-                DataImpl.class,
-                EnumImpl.class
+        var pattern = match(
+                specOf(
+                        DataImpl.class,
+                        EnumImpl.class
                 ),
-                PatternBlock.block(
-                        (Function<DataImpl, Object>) data -> data.value().length(),
-                        (Function<EnumImpl, Object>) Enum::ordinal
-                ));
+                run(
+                        (DataImpl data) -> data.value().length(),
+                        (Function<EnumImpl, Integer>) Enum::ordinal
+                )
+        );
 
-        var dataOut = pattern.against(dataTest);
-        var enumOut = pattern.against(enumImpl);
+        var dataOut = pattern.with(dataTest);
+        var enumOut = pattern.with(enumImpl);
 
         System.out.println("Got this from dataOut! " + dataOut.get());
         System.out.println("And this from enumOut! " + enumOut.get());
